0941ba3fbaf2dee18b0d467976894b71175eb66a
Douglas Goddard <douglas.gastonguay@gmail.com>
2014-10-03 13:31:07 -0400

    That commit got botched. Made exception more generic to handle another crash.

--- src/main/java/com/cisco/CFGScanDroid/ControlFlowGraph.java
+++ src/main/java/com/cisco/CFGScanDroid/ControlFlowGraph.java
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-=======
 /* 	CFGScanDroid - Control Flow Graph Scanning for Android
 	Copyright (C) 2014  Douglas Gastonguay-Goddard
 
@@ -13,7 +11,6 @@
 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 	GNU General Public License for more details */
 
->>>>>>> 5fec7ecbd91ebedd12fab1f173c34afdb90d0131
 package org.talos.CFGScanDroid;
 
 import java.util.Map;
@@ -135,7 +132,7 @@ public class ControlFlowGraph {
 		//List<? extends TryBlock<? extends ExceptionHandler>> tryBlocks = null;
 		if(impl != null) {
 			int address = 0;
-<<<<<<< HEAD
+
 			try {
 				for(Instruction instruction: impl.getInstructions()) {
 					BasicBlockInstruction bbinsn = new BasicBlockInstruction(address, instruction);
@@ -143,15 +140,8 @@ public class ControlFlowGraph {
 					address += instruction.getCodeUnits();
 					flatMethod.add(bbinsn);
 				}
-			} catch(ArrayIndexOutOfBoundsException e) {
+			} catch(Exception e) {
 				return flatMethod;
-=======
-			for(Instruction instruction: impl.getInstructions()) {
-				BasicBlockInstruction bbinsn = new BasicBlockInstruction(address, instruction);
-				//System.out.print("\t" + address + "\t" + instruction.getOpcode() + "\t" + bbinsn.branch);
-				address += instruction.getCodeUnits();
-				flatMethod.add(bbinsn);
->>>>>>> 5fec7ecbd91ebedd12fab1f173c34afdb90d0131
 			}
 			//tryBlocks = impl.getTryBlocks();
 		}
